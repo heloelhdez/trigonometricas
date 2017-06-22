f025c3c6cfd1eba56511c5b50271adabc5ed6905
heloelhdez <heloelhdez@hotmail.com>
2017-06-22 13:11:48 -0500

    Con grados y radianes

--- funciones.js
+++ funciones.js
@@ -1,12 +1,19 @@
 
 function coseno(val) {
-    document.getElementById("coseno").innerHTML = "Resultado: \n Grados: "+toDegrees(Math.cos(val)).toFixed(2)+"\n Radianes: "+Math.cos(val).toFixed(2); 
+    document.getElementById("resultadoC").innerHTML = "Resultado:"
+    document.getElementById("coseno").innerHTML = "Grados: "+toDegrees(Math.cos(val)).toFixed(2);
+    document.getElementById("cosenoR").innerHTML = "Radianes: "+Math.cos(val).toFixed(2);
 }
+
 function seno(val) {
-    document.getElementById("seno").innerHTML = "Resultado: "+toDegrees(Math.sin(val)); 
+    document.getElementById("resultadoS").innerHTML="Resultado:";
+    document.getElementById("seno").innerHTML = "Grados: "+toDegrees(Math.sin(val)).toFixed(2);
+    document.getElementById("senoR").innerHTML = "Radianes: "+Math.sin(val).toFixed(2);
 }
 function tangente(val) {
-    document.getElementById("tangente").innerHTML = "Resultado: "+toDegrees(Math.tan(val)); 
+    document.getElementById("resultadoT").innerHTML = "Resultado:"
+    document.getElementById("tangente").innerHTML = "Grados: "+toDegrees(Math.tan(val)).toFixed(2);
+    document.getElementById("tangenteR").innerHTML = "Radianes: "+Math.tan(val).toFixed(2);
 }
 function toDegrees (angle) {
   return angle *180 / Math.PI;
